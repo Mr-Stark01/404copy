b73953373ac4421cc47bc2907992087444e2bdcc
Stark <stark@stark.com>
2022-02-18 15:06:27 +0100

    upload

--- core/src/com/mygdx/game/units/Knight.java
+++ core/src/com/mygdx/game/units/Knight.java
@@ -8,8 +8,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Knight extends Sprite {
     private final Vector2 velocity = new Vector2();
-    // this is a 2dgame there is no proper gravity dingus
-    private final float speed = 60 *2 ,gravity = 60* 1.8f;
+
 
     public Knight(Sprite sprite){
         super(sprite);
@@ -22,22 +21,13 @@ public class Knight extends Sprite {
     //bs currently
     public void update(float delta) {
         //gravity itself
-        velocity.y -=gravity * delta;
 
-        if (velocity.y> speed){
-            velocity.y=speed;
-        }
-        else if(velocity.y<speed){
-            velocity.y=-speed;
-        }
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
-            velocity.x=speed;
-        }
-        else{
-            velocity.x=-speed;
+            setX(getX()+10);
         }
 
-        setX(getX()+velocity.x *delta);
+
+
         setY(getY()+velocity.x *delta);
     }
 }
