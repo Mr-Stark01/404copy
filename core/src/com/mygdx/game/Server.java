29b73772cb60b697566fef7169aa613c1a7b8d46
stark <gydani01@gmail.com>
2022-02-20 16:43:45 +0100

    asd

--- core/src/com/mygdx/game/Server.java
+++ core/src/com/mygdx/game/Server.java
@@ -14,11 +14,14 @@ public class Server {
             clientSocket = serverSocket.accept();
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String greeting = in.readLine();
-            if ("hello server".equals(greeting)) {
-                out.println("hello client");
-            } else {
-                out.println("unrecognised greeting");
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+                if (".".equals(inputLine)) {
+                    out.println("good bye");
+                    break;
+                }
+                out.println(inputLine);
             }
         }catch (IOException e){
 
