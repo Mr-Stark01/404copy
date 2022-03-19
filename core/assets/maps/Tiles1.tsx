92549b73dc951d54ee2f49ea640276a72a7d72f7
Gergő <gerix0841@gmail.com>
2022-03-19 17:34:29 +0100

    Menu fixed,Join/Host created,Map created

--- core/assets/maps/Tiles1.tsx
+++ core/assets/maps/Tiles1.tsx
@@ -1,6 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="Tiles1" tilewidth="32" tileheight="32" tilecount="784" columns="28">
+<tileset version="1.8" tiledversion="1.8.2" name="Tiles1" tilewidth="32" tileheight="32" tilecount="784" columns="28">
  <image source="../tiles/tiles1_2.png" width="896" height="896"/>
+ <tile id="2">
+  <properties>
+   <property name="blocked" value="blocked"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="CellPath" value="RallyPointServer"/>
+  </properties>
+ </tile>
  <tile id="34">
   <properties>
    <property name="CellPath" value="RallyPointServer"/>
@@ -11,14 +21,34 @@
    <property name="CellPath" value="RallyPointClient"/>
   </properties>
  </tile>
+ <tile id="56">
+  <properties>
+   <property name="blocked" value="blocked"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="blocked" value="blocked"/>
+  </properties>
+ </tile>
  <tile id="60">
   <properties>
    <property name="asd" value="aéldfkga"/>
   </properties>
  </tile>
+ <tile id="62">
+  <properties>
+   <property name="CellPath" value="Client"/>
+  </properties>
+ </tile>
  <tile id="112">
   <properties>
-   <property name="blocked" type="bool" value="true"/>
+   <property name="blocked" value="blocked"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="blocked" value="blocked"/>
   </properties>
  </tile>
 </tileset>
